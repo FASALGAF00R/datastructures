87ceafb1a7eda65fb27b795b5b36feb09a227824
FASALGAFOOR <fasalgafoor2080@gmail.com>
2024-04-04 07:41:37 +0530

    ds working

--- doublell.js
+++ doublell.js
@@ -41,3 +41,47 @@ list.add("Bob");
 list.add("Charlie");
 
 list.display();
+
+
+
+
+
+
+
+
+// // when u create a linked list (head)will be empty and pointer will be null we can change 
+// // this.size property is to track number of nodes in linkedlist
+// isEmpty(){
+// return this.size==0
+// }
+// // isempty is to check whether the linked list is empty
+// getSize(){
+//     return this.size
+// }
+// // to check the size of ll
+// }
+// const list =new linkedlist()
+// console.log('list is empty',list.isEmpty());
+// console.log('list size',list.getSize());
+
+
+// const list = {
+//     head: {
+//         value: 6,
+//         next: {
+//             value: 10        ,                                     
+//             next: {
+//                 value: 12,
+//                 next: {
+//                     value: 3,
+//                     next: null	
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// };
+
+// this represents single nodes
+
+
--- linkedlist.js
+++ linkedlist.js
@@ -1,64 +1,43 @@
+
 class node {
-    constructor(value){
-        this.value=value
-        this.next=null
+    constructor(value) {
+        this.value = value
+        this.next = null
     }
 }
 
-class linkedlist{
- constructor(){
-    this.head=null
- }   
- insertone(data){
-const newnode =new node(data)
-newnode.next=this.head
-this.head=newnode
- }
+class linkedlist {
+    constructor() {
+        this.head = null
+    }
+    insertone(data) {
+        const newnode = new node(data)
+        newnode.next = this.head
+        this.head = newnode
+    }
+
+    print(){
+        let curr=this.head
+        let res='';
+        while(curr!==null){
+            res+=curr.value+'->'
+            curr=curr.next
+        }
+        res+='null';
+        console.log(res);
+    }
+    
+
+
 }
-const ll =new linkedlist()
+const ll = new linkedlist()
 ll.insertone(3)
+ll.insertone(4)
+ll.insertone(5)
 
-console.log(ll);
-
-
-
-
+ll.print()
 
 
 
-// // when u create a linked list (head)will be empty and pointer will be null we can change 
-// // this.size property is to track number of nodes in linkedlist
-// isEmpty(){
-// return this.size==0
-// }
-// // isempty is to check whether the linked list is empty
-// getSize(){
-//     return this.size
-// }
-// // to check the size of ll
-// }
-// const list =new linkedlist()
-// console.log('list is empty',list.isEmpty());
-// console.log('list size',list.getSize());
-
-
-// const list = {
-//     head: {
-//         value: 6,
-//         next: {
-//             value: 10        ,                                     
-//             next: {
-//                 value: 12,
-//                 next: {
-//                     value: 3,
-//                     next: null	
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// };
-
-// this represents single nodes
 
 
